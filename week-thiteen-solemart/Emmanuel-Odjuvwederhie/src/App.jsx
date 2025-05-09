1e25e6ca74c2276c0b6adda19b540704d955f8ba
Brainfriend4life <emmanueloghenevovwero85@gmail.com>
2025-05-09 17:07:04 +0100

    add index.html file

---
+++ week-thiteen-solemart/Emmanuel-Odjuvwederhie/src/App.jsx
@@ -0,0 +1,13 @@
+import NavBar from "./components/NavBar";
+import Body from "./components/Body";
+
+const App = () => {
+  return (
+    <>
+      <NavBar />
+      <Body />
+    </>
+  );
+};
+
+export default App;
